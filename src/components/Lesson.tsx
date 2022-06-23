9eb7281ea7f54c8c6d5d0f0fb2c528f9f5879cc1
helineto10 <heli.neto10@gmail.com>
2022-06-23 18:44:36 -0300

    Fim da aula 3, criação da parte do video

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast , format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -15,12 +16,12 @@ export default function Lesson(props: LessonProps) {
   const availableDateFormat = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {locale : ptBR})
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-grey-300">
         {availableDateFormat}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -40,6 +41,6 @@ export default function Lesson(props: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
